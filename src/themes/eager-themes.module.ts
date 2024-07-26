26f8800d92801f3cc60977959ed3045450b610b8
eulereadgbe <esnemiz@gmail.com>
2024-07-26 13:55:53 +0800

    Add initial NFRDI theme using Cosmo theme from Bootswatch

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as NFRDIEagerThemeModule } from './nfrdi/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
@@ -11,7 +11,7 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
+    NFRDIEagerThemeModule,
     // CustomEagerThemeModule,
   ],
 })
