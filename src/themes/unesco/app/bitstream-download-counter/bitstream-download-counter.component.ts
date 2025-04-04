d84713bf2fd9e3ba59b4f7d1725f6636dd94c2c7
eulereadgbe <esnemiz@gmail.com>
2025-04-04 21:15:40 +0800

    Revert "Hide download counter if count = 0, format download number to x,xxx"
    This reverts commit fcd8a89fe4201db9259ba6491a2564d5f5b67116.

--- src/themes/unesco/app/bitstream-download-counter/bitstream-download-counter.component.ts
+++ src/themes/unesco/app/bitstream-download-counter/bitstream-download-counter.component.ts
@@ -3,40 +3,33 @@
   // Bitstream download displays.
   import { Component, Input, OnInit, inject } from '@angular/core';
   import { Bitstream } from '../../../../app/core/shared/bitstream.model';
-  import { Observable, map } from 'rxjs'; // Import the map operator
+  import { Observable } from 'rxjs';
   import { UsageReportDataService } from '../../../../app/core/statistics/usage-report-data.service';
   import { UsageReport } from '../../../../app/core/statistics/models/usage-report.model';
-  import { AsyncPipe, DecimalPipe } from '@angular/common';
+  import { AsyncPipe } from '@angular/common';
 
   @Component({
     selector: 'ds-bitstream-download-counter',
-    template: `{{ label }} {{ downloadCount | number: '1.0-0' }}`, // Format with DecimalPipe
+    template: `{{ label }} {{ (downloadsReport$ | async)?.points[0]?.values?.views }}`,
     standalone: true,
-    imports: [AsyncPipe, DecimalPipe],
+    imports: [AsyncPipe],
   })
   export class BitstreamDownloadCounterComponent implements OnInit {
     @Input() bitstream: Bitstream;
     @Input() label: string;
 
-    downloadsReport$: Observable<number>;
+    downloadsReport$: Observable<UsageReport>;
     usageReportService = inject(UsageReportDataService);
-    downloadCount: number = 0;
 
-  ngOnInit(): void {
-    if (this.bitstream?.id) {
-      this.downloadsReport$ = this.usageReportService.getStatistic(
-        this.bitstream.id,
-        'TotalDownloads'
-      ).pipe(
-        map(report => report?.points[0]?.values?.[0]?.views || 0) // Extract count and optional chaining
-      );
-
-      this.downloadsReport$.subscribe(count => {
-        this.downloadCount = count;
-      });
+    ngOnInit(): void {
+      if (this.bitstream?.id) {
+        this.downloadsReport$ = this.usageReportService.getStatistic(
+          this.bitstream.id,
+          'TotalDownloads'
+        );
       } else {
         console.warn('Bitstream ID is missing, cannot fetch download statistics.');
-        this.downloadsReport$ = new Observable<number>(); // Or a default empty observable
+        this.downloadsReport$ = new Observable<UsageReport>(); // Or a default empty observable
       }
     }
   }
--- src/themes/unesco/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/unesco/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { FileSectionComponent as BaseComponent } from '../../../../../../../app/item-page/simple/field-components/file-section/file-section.component';
@@ -28,15 +28,6 @@ import { BitstreamDownloadCounterComponent } from '../../../../bitstream-downloa
     BitstreamDownloadCounterComponent, // Add the imported component to the imports array
   ],
 })
-export class FileSectionComponent extends BaseComponent implements AfterViewInit {
-  @ViewChildren('downloadCounter') downloadCounterComponents: QueryList<BitstreamDownloadCounterComponent>;
-  downloadCounterComponentMap: { [key: string]: BitstreamDownloadCounterComponent } = {};
+export class FileSectionComponent extends BaseComponent {
 
-  ngAfterViewInit(): void {
-    this.downloadCounterComponents.forEach(component => {
-      if (component.bitstream?.id) {
-        this.downloadCounterComponentMap[component.bitstream.id] = component;
-      }
-    });
-  }
 }
