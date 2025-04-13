6e5a1451f4cc326119952086a3933346b839207e
eulereadgbe <esnemiz@gmail.com>
2025-04-13 18:36:27 +0800

    Add a custom SDG component

--- src/themes/upv/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ src/themes/upv/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -29,6 +29,7 @@ import { MetadataFieldWrapperComponent } from '../../../../../../../app/shared/m
 import { listableObjectComponent } from '../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ThemedResultsBackButtonComponent } from '../../../../../../../app/shared/results-back-button/themed-results-back-button.component';
 import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/themed-thumbnail.component';
+import { ItemPageSdgFieldComponent } from '../../../../../../../app/item-page/simple/field-components/specific-field/sdg/item-page-sdg-field.component';
 
 /**
  * Component that represents an untyped Item page
@@ -65,6 +66,7 @@ import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/the
     AsyncPipe,
     TranslateModule,
     ItemPageCcLicenseFieldComponent,
+    ItemPageSdgFieldComponent,
   ],
 })
 export class UntypedItemComponent extends BaseComponent {}
