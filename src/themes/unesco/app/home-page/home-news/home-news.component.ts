f62a14b92a2ae577dd6b9de0dbc768d7b163081b
eulereadgbe <esnemiz@gmail.com>
2025-05-05 19:03:52 +0800

    Removed slideshow, just display static banner image at the home news page.

--- src/themes/unesco/app/home-page/home-news/home-news.component.ts
+++ src/themes/unesco/app/home-page/home-news/home-news.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,73 +14,9 @@ import { CommonModule } from '@angular/common';
 /**
  * Component to render the news section on the home page
  */
-export class HomeNewsComponent implements OnInit, OnDestroy {
-  slides = [
-    {
-      image: 'assets/unesco/images/home-news/banner1.jpg', // Fallback JPG
-      imageWebp: 'assets/unesco/images/home-news/banner1.webp',
-      credit: 'Gregory Piper / Ocean Image Bank',
-      link: 'https://www.theoceanagency.org/search-result?img=YHc69BMAACMArtvv'
-    },
-    {
-      image: 'assets/unesco/images/home-news/banner2.jpg', // Fallback JPG
-      imageWebp: 'assets/unesco/images/home-news/banner2.webp',
-      credit: 'Alex Tyrrell / Ocean Image Bank',
-      link: 'https://www.theoceanagency.org/search-result?img=YEgD9BEAACIAqMSi'
-    },
-    {
-      image: 'assets/unesco/images/home-news/banner3.jpg', // Fallback JPG
-      imageWebp: 'assets/unesco/images/home-news/banner3.webp',
-      credit: 'Liang Fu / Ocean Image Bank',
-      link: 'https://www.theoceanagency.org/search-result?img=YGXmIBMAACAAYN4S'
-    },
-    {
-      image: 'assets/unesco/images/home-news/banner4.jpg', // Fallback JPG
-      imageWebp: 'assets/unesco/images/home-news/banner4.webp',
-      credit: 'Jayne Jenkins / Ocean Image Bank',
-      link: 'https://www.theoceanagency.org/search-result?img=YHTveRMAACAApIuf'
-    },
-    {
-      image: 'assets/unesco/images/home-news/banner5.jpg', // Fallback JPG
-      imageWebp: 'assets/unesco/images/home-news/banner5.webp',
-      credit: 'Ocean Image Bank',
-      link: 'https://www.theoceanagency.org/search-result?img=YGD5KhIAACAAmO_q'
-    }
-  ];
-
-  currentSlide = 0;
-  private slideshowIntervalId: any;
-
-  ngOnInit(): void {
-    this.startAutoPlay();
-  }
-
-  ngOnDestroy(): void {
-    clearInterval(this.slideshowIntervalId);
-  }
-
-  startAutoPlay(): void {
-    this.slideshowIntervalId = setInterval(() => {
-      this.nextSlide(false);
-    }, 10000);
-  }
-
-  nextSlide(resetTimer: boolean = true): void {
-    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-    if (resetTimer) this.resetAutoPlay();
-  }
-
-  previousSlide(): void {
-    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
-    this.resetAutoPlay();
-  }
-
-  resetAutoPlay(): void {
-    clearInterval(this.slideshowIntervalId);
-    this.startAutoPlay();
-  }
-
-  get currentCredit(): string {
-    return this.slides[this.currentSlide]?.credit || '';
-  }
+export class HomeNewsComponent {
+  bannerCredit = {
+    credit: 'Gregory Piper / Ocean Image Bank',
+    link: 'https://www.theoceanagency.org/search-result?img=YHc69BMAACMArtvv'
+  };
 }
