7f91bc8132ef4202d86d626be7a571f14f1fc602
eulereadgbe <esnemiz@gmail.com>
2025-10-21 01:04:39 +0800

    Initial theming

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+//import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
-
+import { EagerThemeModule as PLAIEagerThemeModule } from './plai/eager-theme.module';
 /**
  * This module bundles the eager theme modules for all available themes.
  * Eager modules contain components that are present on every page (to speed up initial loading)
@@ -12,7 +12,7 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
+    PLAIEagerThemeModule,
     // CustomEagerThemeModule,
   ],
 })
