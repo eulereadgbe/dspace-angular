80e18a4b142107747b91b2861590ca9114935b19
eulereadgbe <esnemiz@gmail.com>
2025-11-25 14:45:36 +0800

    Modified simple item view for untyped items

--- src/themes/plai/eager-theme.module.ts
+++ src/themes/plai/eager-theme.module.ts
@@ -5,28 +5,29 @@ import { RootModule } from '../../app/root.module';
 import { HeaderComponent } from './app/header/header.component';
 import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
 import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
+import { UntypedItemComponent } from './app/item-page/simple/item-types/untyped-item/untyped-item.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
 
 /**
  * Add components that use a custom decorator to ENTRY_COMPONENTS as well as DECLARATIONS.
  * This will ensure that decorator gets picked up when the app loads
  */
-const ENTRY_COMPONENTS = [];
-
-const DECLARATIONS = [
-  ...ENTRY_COMPONENTS,
-  HomeNewsComponent,
-  HeaderComponent,
-  HeaderNavbarWrapperComponent,
-  NavbarComponent,
+const ENTRY_COMPONENTS = [
+  UntypedItemComponent,
 ];
+const DECLARATIONS = [];
 
 @NgModule({
   imports: [
     CommonModule,
     RootModule,
-    ...DECLARATIONS,
+    UntypedItemComponent,
+    HomeNewsComponent,
+    HeaderComponent,
+    HeaderNavbarWrapperComponent,
+    NavbarComponent,
   ],
+  declarations: DECLARATIONS,
   providers: [
     ...ENTRY_COMPONENTS.map((component) => ({ provide: component })),
   ],
